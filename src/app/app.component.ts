7bfe2fe6239b67962d0484816f7c9cd6d347dfa0
Renato Probst <renato@rockapps.com.br>
2020-06-26 09:13:11 -0300

    full project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+// import 'three';
+// import 'aframe';
+ import 'aframe-environment-component';
+import 'aframe-google-poly-component';
+// import 'aframe-ar';
 
 @Component({
   selector: 'app-root',
